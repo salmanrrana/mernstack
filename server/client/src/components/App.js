2c4cbce35cdf55605f8c0d0384097db4f2170167
Salman Rana <salman.r.rana@gmail.com>
2017-08-20 16:24:26 -0500

    added authentication on nav bar for frontend and backend

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return (
-    <div>
-      IF you smell what the rock is coooooooookin
-    </div>
-  );
-};
 
-export default App;
+import Header from "./Header";
+import Landing from './Landing';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
